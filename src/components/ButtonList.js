4009963da3ec44a20a1ec49602f98aae35c48d51
prikshit8 <prikshitchawla.v@gmail.com>
2024-04-13 10:08:07 +0530

    api key setup

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,4 +1,26 @@
+import Button from "./Button";
+
+const names = [
+  "All",
+  "Gaming",
+  "Songs",
+  "Live",
+  "Cricket",
+  "Soccer",
+  "News",
+  "Cooking",
+  "Frontend",
+  "React",
+  "UI",
+];
+
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      {names.map((name) => (
+        <Button key={name} name={name} />
+      ))}
+    </div>
+  );
 };
 export default ButtonList;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+
 const VideoContainer = () => {
+  useEffect(() => {
+    getVideos();
+  }, []);
+  const getVideos = async () => {
+    const response = await fetch(YOUTUBE_VIDEOS_API);
+    const data = await response.json();
+    console.log(data);
+  };
   return <div>video container</div>;
 };
 export default VideoContainer;

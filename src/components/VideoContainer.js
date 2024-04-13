85aa296dafb1b446c8c85c12d9a31ba6cbefe02e
prikshit8 <prikshitchawla.v@gmail.com>
2024-04-13 10:39:19 +0530

    homepage

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,15 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
     getVideos();
   }, []);
   const getVideos = async () => {
     const response = await fetch(YOUTUBE_VIDEOS_API);
     const data = await response.json();
-    console.log(data);
+    setVideos(data.items);
   };
-  return <div>video container</div>;
+  return (
+    <div className="flex flex-wrap">
+      {videos.length > 0 && videos.map((video) => <VideoCard info={video} />)}
+    </div>
+  );
 };
 export default VideoContainer;
